e425a3acecacd320d5888245acca513223949ef5
Борис Сергеевич <117807416+TheDekeract@users.noreply.github.com>
2022-12-07 13:39:05 +0300

    Add files via upload

--- 7-8. 14.pas
+++ 7-8. 14.pas
@@ -6,9 +6,11 @@ begin
   writeln('Введите строку: ');
   read(str);
   i := Pos('word', str);
+  while i <> 0 do
   if i > 0 then begin
     delete(str, i, 4);
     Insert('letter', str, i);
+    i:=Pos('word', str)
   end;
   writeln(str);
 end.
